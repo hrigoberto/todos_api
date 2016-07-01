59b8e6abbd4a2ee1bf7cd76c65ed190f3a2a006a
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-07-01 19:14:45 -0400

    setup routes

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ var express = require('express');
 var server = (express());
 var port = process.env.PORT || 8080
 
-server.use(express.static(__dirname+'/public'));
+server.get('/todos', function(request, response){
+  response.send("GET todos");
+})
 
-server.listen(port);
+server.get('/todos/:id', function(request, response){
+  response.send("GET todos :id");
+})
+
+server.post('/todos', function(request, response){
+  response.send("POST todos");
+})
+
+server.put('/todos/:id', function(request, response){
+  response.send("PUT todos :id");
+})
+
+server.delete('/todos/:id', function(request, response){
+  response.send("DELETE todos :id");
+})
+
+server.listen(port, function(){
+  console.log('Now listening to port:', port);
+});
