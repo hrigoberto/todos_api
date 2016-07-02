7e34e4d6dc09f2e3ad735abf4a8c18f73081928f
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-07-01 20:37:33 -0400

    setup post route with unique id

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var server = (express());
+var bodyParser = require('body-parser');
+var lowdb = require('lowdb');
+var uuid = require('uuid');
+
 var port = process.env.PORT || 8080
+var db = lowdb('db.json');
+
+// database initialization
+db.defaults({todos: []})
+// .value() runs the previous set of commands
+  .value();
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 server.get('/todos', function(request, response){
   response.send("GET todos");
@@ -11,8 +24,18 @@ server.get('/todos/:id', function(request, response){
 })
 
 server.post('/todos', function(request, response){
-  response.send("POST todos");
-})
+  var todo = {
+    id: uuid.v4(),
+    description: request.body.description,
+    isComplete: false
+  };
+
+  var result = db.get('todos')
+                 .push(todo)
+                 .last()
+                 .value();
+  response.send(result);
+});
 
 server.put('/todos/:id', function(request, response){
   response.send("PUT todos :id");
